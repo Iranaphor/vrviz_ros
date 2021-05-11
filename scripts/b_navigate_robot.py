5cd7fa80944b3c6d8900111c56b4b75691d8c144
Iranaphor <Primordia@live.com>
2021-05-11 01:16:46 +0100

    RunCom modifications

--- scripts/b_navigate_robot.py
+++ scripts/b_navigate_robot.py
@@ -4,7 +4,8 @@ import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-goal_list = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+goal_list = [(-1,3), (-4.5, 3), (0, 1), (4.5, -2), (1, 0), (2, -4), (1, 1), (0, -1), (-1, 0)]
+# goal_list = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 def format_goal_obj(pos):
