6081a22ade495516d9f182cd04e5230d68a3d94f
Iranaphor <Primordia@live.com>
2021-05-17 00:37:43 +0100

    Reshuffling of launching

--- scripts/b_navigate_robot.py
+++ scripts/b_navigate_robot.py
@@ -4,7 +4,7 @@ import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-goal_list = [(-1,3), (3, -4.5), (0, 1), (4.5, -2), (1, 0), (2, -4), (1, 1), (0, -1), (-1, 0)]
+goal_list = [(-1,3), (0, 1), (4.5, -2), (1, 0), (2, -4), (1, 1), (0, -1), (-1, 0)]
 # goal_list = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
